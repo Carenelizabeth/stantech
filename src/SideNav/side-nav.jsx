a03cc28984867013a2bfca431735f70b6f64f95b
Caren Keyes <cejkeyes@gmail.com>
2019-04-06 18:49:30 -0400

    add dummy sidelinks

--- src/SideNav/side-nav.jsx
+++ src/SideNav/side-nav.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './side-nav.css';
 
 export default function SideNav(props){
     if(props.visible){
         return(
             <div className='sidenav-wrapper'>
-                <h2>Side Bar</h2>
+                <div className='sidebar-links'>
+                    <Link to='/projects'>Projects</Link>
+                    <Link to='/about'>About Us</Link>
+                    <Link to='/something'>Something Else</Link>
+                </div>
             </div>
         )
     }
